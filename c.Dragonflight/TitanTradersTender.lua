ceec9b921c1d9f23bc207cc99a4852b7cd7e65cb
Zetaprime82 <darklord_dm_@hotmail.com>
2023-02-03 10:16:23 +0100

    added Trader's Tender

--- c.Dragonflight/TitanTradersTender.lua
+++ c.Dragonflight/TitanTradersTender.lua
@@ -1,18 +1,18 @@
 --[[
-Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your Bloody Tokens.
+Description: This plugin is part of the "Titan Panel [Currencies] Multi" addon. It shows your Trader's Tender.
 Site: https://www.curseforge.com/wow/addons/titan-panel-currencies-multi
 Author: Canettieri
 Special Thanks to Eliote.
 --]]
 
 local _, L = ...;
-local ID = "TITAN_DFBLYM"
-local CURRENCY_ID = 2123
+local ID = "TITAN_TST"
+local CURRENCY_ID = 2032
 
 L:CreateSimpleCurrencyPlugin({
 	currencyId = CURRENCY_ID,
 	titanId = ID,
-	noCurrencyText = L["DragonFPvP"],
+	noCurrencyText = L["DragonFOnly"],
 	expName = L["mDragonflight"],
 	category = "CATEGORY_DRAGON"
 })
